b4a9df68c9fab14c1896a82ce78fa330588005d8
RCASTUedaLab <ueda@biods.rcast.u-tokyo.ac.jp>
2022-05-30 15:00:21 +0900

    change network

--- nanoDoc2_1/utils/PlotGraph.py
+++ nanoDoc2_1/utils/PlotGraph.py
@@ -38,9 +38,9 @@ def plotGraph(fin,out,ans,figsize=(23,4)):
     answer = getAns(ans)
     scores = getData(fin)
     plt.figure(figsize=figsize)
-    plt.plot(scores,lw=1)
+    plt.plot(scores,lw=2)
     for xc in answer:
-        plt.axvline(xc, color='red', alpha=.25)
+        plt.axvline(xc, color='red', alpha=.25,lw=2)
 
     plt.savefig(out)
     plt.ylim(-0.1, 1)
@@ -60,10 +60,10 @@ if __name__ == "__main__":
 
     # fin = '/data/nanopore/nanoDoc2_1/varidate/16S_score.txt'
     # ans = '/data/nanopore/nanoDoc2_1/testrun/16sans.txt'
-    # out = '/data/nanopore/nanoDoc2_1/varidate/16sout.png'
+    # out = '/data/nanopore/nanoDoc2_1/varidate/16sout2.png'
     # figsize = (16, 4)
     fin = '/data/nanopore/nanoDoc2_1/varidate/23S_score.txt'
     ans = '/data/nanopore/nanoDoc2_1/testrun/23sans.txt'
-    out = '/data/nanopore/nanoDoc2_1/varidate/23sout.png'
+    out = '/data/nanopore/nanoDoc2_1/varidate/23sout2.png'
     figsize = (23, 4)
     plotGraph(fin,out,ans,figsize)
